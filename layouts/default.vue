c5a27b3ac4eef320d5c19409b6f20a9106ba2bb5
Benjamart453956 <97876506+Benjamart453956@users.noreply.github.com>
2024-02-08 15:52:11 +0000

    add 2nd_installment feature

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,10 +13,10 @@
                         </a>
                     </UDropdown>
                     <a>
-                        <NuxtLink to="/track/letter">ลูกหนี้งวดที่ 1</NuxtLink>
+                        <NuxtLink to="/">ลูกหนี้งวดที่ 1</NuxtLink>
                     </a>
                     <a>
-                        <NuxtLink to="/track/letter2">ลูกหนี้งวดที่ 2</NuxtLink>
+                        <NuxtLink to="/track/2nd_installment">ลูกหนี้งวดที่ 2</NuxtLink>
                     </a>
                     <a>
                         <NuxtLink to="/">ลูกหนี้ NPL</NuxtLink>
